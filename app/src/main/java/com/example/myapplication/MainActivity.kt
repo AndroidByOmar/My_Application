afa59d8b58fa15d40664a429c827c3fe3d11a5e4
omarabdelsalm <93231975za@gmail.com>
2022-11-19 20:46:27 +0200

    add new widget like image

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
  class MainActivity : AppCompatActivity() {
@@ -14,8 +15,12 @@ import android.widget.Toast
         setContentView(R.layout.activity_main)
 
        val myclick = findViewById<Button>(R.id.button)
+        val textView=findViewById<TextView>(R.id.textView)
+        val txt=findViewById<TextView>(R.id.txt22)
         myclick.setOnClickListener {
-            Toast.makeText(this,"my event",Toast.LENGTH_SHORT).show()
+           // Toast.makeText(this,"my event",Toast.LENGTH_SHORT).show()
+            var mem=txt.text.toString()
+            textView.text=mem.toString()
         }
         Toast.makeText(this,"my app",Toast.LENGTH_SHORT).show()
         myclick.setOnLongClickListener(){
@@ -23,7 +28,7 @@ import android.widget.Toast
             true
         }
 
-    }
+    }//End fun of Oncreate
 
 
 }
