efa7e73487e80d596de146632d1dd4385ea2ac19
omarabdelsalm <93231975za@gmail.com>
2022-11-24 20:45:51 +0200

    اضافة اكتيفتي جديد مع صفحة جديدة مع تيم بيكر

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,34 +1,42 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       val myclick = findViewById<Button>(R.id.button)
+       val myNewEvent = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.button2)
         val textView=findViewById<TextView>(R.id.textView)
         val txt=findViewById<TextView>(R.id.txt22)
-        myclick.setOnClickListener {
+        myNewEvent.setOnClickListener {
            // Toast.makeText(this,"my event",Toast.LENGTH_SHORT).show()
-            var mem=txt.text.toString()
-            textView.text=mem.toString()
+            val mem=txt.text.toString()
+            textView.text=mem
         }
         Toast.makeText(this,"my app",Toast.LENGTH_SHORT).show()
-        myclick.setOnLongClickListener(){
+        myNewEvent.setOnLongClickListener{
             Toast.makeText(this,"my long event",Toast.LENGTH_SHORT).show()
             true
         }
+  // add timepicker
+        button.setOnClickListener {
+            val intint= Intent(this,SecandActivity::class.java)
+            startActivity(intint)
+        }
 
-    }//End fun of Oncreate
+    }//End fun of create_Home
 
 
 }
